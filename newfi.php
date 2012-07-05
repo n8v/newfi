237b3b8e3037c8feec88fb689b172c41e91fa07e
Nathan Vonnahme <nathan.vonnahme@bannerhealth.com>
2012-07-05 14:01:22 -0800

    Try more. Flock more.

--- newfi.php
+++ newfi.php
@@ -5,6 +5,8 @@ header('Content-type: text/html');
 //$datafile = dirname($_SERVER{'SCRIPT_FILENAME'}) . '/newfi';
 $datafile = './newfi';
 
+$maxtries = 9;
+$tries = 0;
 
 if (isset($_REQUEST['s'])) {
    $timeday = array();
@@ -12,15 +14,29 @@ if (isset($_REQUEST['s'])) {
 
    $say = strftime("%Y%m%d %H%M%S") . "." . $timeday["usec"] . ": " . $_REQUEST['s'] . "\r\n";
 
-   $file = fopen($datafile,"a");
-//   fseek($file,0,SEEK_END);
+   $file = false;
+   do {
+       $file = @fopen($datafile,"a");
+       //   fseek($file,0,SEEK_END);
+       if (!$file) usleep(1000); // microseconds
+
+   }
+   while (!$file && $tries <= $maxtries);
 
    if ($file) {
-       fwrite($file,$say,strlen($say));
-
-       fclose($file);
+       if (flock($file, LOCK_EX)) {
+	   fwrite($file,$say,strlen($say));
+	   fclose($file);
+       }
+       else {
+	       die( "COULD NOT FLOCK $datafile");
+       }
    }
-
+   else {
+       echo "COULD NOT OPEN $datafile FOR APPENDING after $tries tries!";
+       exit;
+   }
+       
 }
 
 function pretty($string)
@@ -37,29 +53,40 @@ function pretty($string)
     return implode('',$newstring);
 }
 
+$maxtries = 9;
+$tries = 0;
+
 if (isset($_REQUEST['g'])) {
     $get = intval($_REQUEST['g']);
     
     $file = fopen($datafile,"r");
     if ($file) {
-	do {
-	    //      $read = array($file);
-	    //      $write = NULL;
-	    //      $except = NULL;
-	    fseek($file,$get,0);
-	    //      stream_select($read,$write,$except,1);
-	    $data = fread($file,1000000);
-	    $newsize = ftell($file);
-	    if ($newsize == $get) usleep(100000);
-	} while($newsize == $get);
-	fclose($file);
+	
+	if (flock($file, LOCK_SH)) {
 
-	$data = pretty($data);
+	    do {
+		//      $read = array($file);
+		//      $write = NULL;
+		//      $except = NULL;
+		fseek($file,$get,0);
+		//      stream_select($read,$write,$except,1);
+		$data = fread($file,1000);
+		$newsize = ftell($file);
+		if ($newsize == $get) usleep(100000);
+		$tries++;
+	    } while($newsize == $get && $tries <= $maxtries);
+	    fclose($file);
+
+	    $data = pretty($data);
     
-	echo $newsize . ";" . $data;
+	    echo $newsize . ";" . $data;
+	}
+	else {
+	    die("COULD NOT aquire shared lock.");
+	}
     }
     else {
-	echo "ERRAR";
+	die("ERRAR. Could not open $datafile for reading.");
     }
 
 }
